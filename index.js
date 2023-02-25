bea14ba0e08672a19e970acf41f2396d3a21a386
unknown <aniljassi88@gmail.com>
2023-02-25 13:36:08 +0000

    installed npm files and enquirer

--- index.js
+++ index.js
@@ -9,63 +9,66 @@ const questions = [
         type: "input",
         name: "title",
         message: "Please name the title of your project?",
-    }
+    },
     {
         type: "input",
         name: "title",
         message: "Please name the sections?",
-    }
+    },
     {
         type: "input",
         name: "title",
         message: "Please describe your project?",
-    }
+    },
     {
         type: "input",
         name: "title",
         message: "Please input your table of contents?",
-    }
+    },
     {
         type: "input",
         name: "title",
         message: "what type of installation will this be?",
-    }
+    },
     {
         type: "input",
         name: "title",
         message: "what will this be used for?",
-    }
+    },
     {
         type: "input",
         name: "title",
         message: "What license are you using for this?",
-    }
+    },
     {
         type: "input",
         name: "title",
         message: "Who is contributing to this project?",
-    }
+    },
     {
         type: "input",
         name: "title",
         message: "what tests will be done?",
-    }
+    },
     {
         type: "input",
         name: "title",
         message: "What questions will this have?",
-    }
+    },
 
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(preocess.cwd(),fileName),data)
 }
 
 // function to initialize program
 function init() {
-
-}
+inquirer.createPromptModule(questions).then((responses)) -> {
+console.log("creating README.md file...");
+writeToFile("./README.md");
+}}
 
 // function call to initialize program
 init();
