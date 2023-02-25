27dc0fab563d4d69f37fe7d2e55d4fc5d15511ed
unknown <aniljassi88@gmail.com>
2023-02-25 13:18:21 +0000

    added questions in index file

--- index.js
+++ index.js
@@ -1,10 +1,60 @@
 const fs = require("fs");
 const path = require('path');
 const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown");
+const generateMarkdown = require("./generateMarkdown");
 
 // array of questions for user
 const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "Please name the title of your project?",
+    }
+    {
+        type: "input",
+        name: "title",
+        message: "Please name the sections?",
+    }
+    {
+        type: "input",
+        name: "title",
+        message: "Please describe your project?",
+    }
+    {
+        type: "input",
+        name: "title",
+        message: "Please input your table of contents?",
+    }
+    {
+        type: "input",
+        name: "title",
+        message: "what type of installation will this be?",
+    }
+    {
+        type: "input",
+        name: "title",
+        message: "what will this be used for?",
+    }
+    {
+        type: "input",
+        name: "title",
+        message: "What license are you using for this?",
+    }
+    {
+        type: "input",
+        name: "title",
+        message: "Who is contributing to this project?",
+    }
+    {
+        type: "input",
+        name: "title",
+        message: "what tests will be done?",
+    }
+    {
+        type: "input",
+        name: "title",
+        message: "What questions will this have?",
+    }
 
 ];
 
